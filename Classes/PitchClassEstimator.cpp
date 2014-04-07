0798a1e9fcb26a2d49d375521db080f48db4de2d
e7mac <mayank.ot@gmail.com>
2014-04-06 17:30:15 -0700

    added other files (untested)

---
+++ Classes/PitchClassEstimator.cpp
@@ -0,0 +1,13 @@
+//
+//  PitchClassEstimator.cpp
+//  DSPLibrary
+//
+//  Created by Mayank on 9/11/12.
+//  Copyright (c) 2012 Mayank Sanganeria. All rights reserved.
+//
+
+#include "PitchClassEstimator.h"
+
+PitchClassEstimator::PitchClassEstimator()
+{
+}
---
+++ Classes/PitchClassEstimator.h
@@ -0,0 +1,22 @@
+//
+//  PitchClassEstimator.h
+//  DSPLibrary
+//
+//  Created by Mayank on 9/11/12.
+//  Copyright (c) 2012 Mayank Sanganeria. All rights reserved.
+//
+
+#ifndef __DSPLibrary__PitchClassEstimator__
+#define __DSPLibrary__PitchClassEstimator__
+
+#include <iostream>
+
+class PitchClassEstimator {
+public:
+    int buffer[1000000];
+public:
+    PitchClassEstimator();
+};
+
+
+#endif /* defined(__DSPLibrary__PitchClassEstimator__) */
